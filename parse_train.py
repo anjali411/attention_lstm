c345c85fe43f14083d56c9eaba8471851ddcb466
Anjali <chourdiaanjali123@gmail.com>
2018-12-23 14:59:26 -0500

    parse  train

--- parse_train.py
+++ parse_train.py
@@ -10,11 +10,11 @@ train_data_word2idx = {}
 train_data_aspect2idx = {}
 word_count = 0
 aspect_count = 0
+neut = 0
 neg = 0
 pos = 0
 train_data_wordvec = bcolz.carray(np.zeros(1), rootdir = 'word_vec.dat', mode='w')
 train_data_aspectvec = bcolz.carray(np.zeros(1), rootdir = 'aspect_vec.dat', mode='w')
-
 
 vectors = bcolz.open('6B.50.dat')[:]
 word2idx = pickle.load(open('6B.50_idx.pkl', 'rb'))
@@ -22,7 +22,7 @@ word2idx = pickle.load(open('6B.50_idx.pkl', 'rb'))
 existing_words = 0
 existing_aspect_words = 0
 
-non_neut_e, non_neut_a=0, 0
+_e, _a=0, 0
 turn = 0
 
 for i in range(0, len(lines), 3):
@@ -35,15 +35,15 @@ for i in range(0, len(lines), 3):
 		if word not in train_data_word2idx.keys():
 			if word in word2idx.keys():
 				vec = vectors[word2idx[word]]
-				if(int(polarity) != 0):
+				if(int(polarity) != 1 or turn ==1):
 					existing_words+=1
 			else:
 				vec = np.random.uniform(low=-1, high = 1, size = (50,))
 
 			train_data_word2idx[word] = word_count
 			word_count+=1
-			if(int(polarity) != 0):
-				non_neut_e+=1
+			if(int(polarity) != 1 or turn ==1):
+				_e+=1
 			#train_data_wordvec = np.concatenate((train_data_wordvec,vec))
 			train_data_wordvec.append(vec)
 			# print(word, word_count, "\n")
@@ -52,7 +52,7 @@ for i in range(0, len(lines), 3):
 		if word not in train_data_aspect2idx.keys():
 			if word in word2idx.keys():
 				vec_ = vectors[word2idx[word]]
-				if(int(polarity) != 0):
+				if(int(polarity) != 1 or turn ==1):
 					existing_aspect_words+=1
 			else:
 				print("aspect word: ", word)
@@ -60,24 +60,28 @@ for i in range(0, len(lines), 3):
 
 			train_data_aspect2idx[word] = aspect_count
 			aspect_count+=1
-			if(int(polarity) != 0):
-				non_neut_a+=1
+			if(int(polarity) != 1 or turn ==1):
+				_a+=1
 			#train_data_aspectvec = np.concatenate((train_data_aspectvec,vec_))
 			train_data_aspectvec.append(vec_)
 	
 	#train_input.append([words, aspect_words, int(polarity)])
-	if(int(polarity) != 0):
-		if(int(polarity) == 1):
-			if(turn == 1):
-				train_input.append([words, aspect_words, int(polarity)])
-				pos+=1
-			turn = 1-turn
-		elif(int(polarity) == -1):
-			train_input.append([words, aspect_words, int(polarity)])
-			neg+=1
+	if(int(polarity) != 1 or turn ==1):
+		train_input.append([words, aspect_words, int(polarity)])
+			
+	if(int(polarity) == 1):
+		turn = 1-turn
+	
+	if(int(polarity) == 0):
+		neut+=1
+	elif(int(polarity) == -1):
+		neg+=1
+	elif(turn == 1):
+		pos+=1
 
 print("pos: ", pos)
 print("neg: ", neg)
+print("neut: ", neut)
 
 vec_ = np.random.uniform(low=-1, high = 1, size = (50,))
 train_data_word2idx['<UKW>'] = word_count
@@ -98,7 +102,7 @@ train_data_aspect2idx['<PAD>'] = aspect_count+1
 word_count+=2
 aspect_count+=2
 
-print("Percentage of existing words: ",existing_words/non_neut_e, "\nPercentage of existing aspect words: ",existing_aspect_words/non_neut_a)
+print("Percentage of existing words: ",existing_words/_e, "\nPercentage of existing aspect words: ",existing_aspect_words/_a)
 
 train_data_wordvec = bcolz.carray(train_data_wordvec[1:].reshape((word_count, 50)), rootdir = '50_word_vec.dat', mode='w')
 train_data_wordvec.flush()
@@ -112,8 +116,4 @@ train_data_aspectvec.flush()
 
 pickle.dump(train_data_word2idx, open('50_train_data_word2idx.pkl', 'wb'))
 pickle.dump(train_data_aspect2idx, open('50_train_data_aspect2idx.pkl', 'wb'))
-pickle.dump(train_input, open('without_neut_train_input.pkl', 'wb'))
-
-
-#73%, 71%...
-#89%, ...
+pickle.dump(train_input, open('neut_neg_pos_train_input.pkl', 'wb'))
